8845e56c7a9122d111fa6e7c6c44aacd2f8ba6da
tiago.lpsmn <tiago.lopesmonteiro@eplf.ch>
2025-04-23 22:54:14 +0200

    Wrong file added on previous commit

--- combining_block.py
+++ combining_block.py
@@ -20,9 +20,9 @@ for _, row in whisper_output.iterrows():
 
 seperated_whisper_df = pd.DataFrame(seperated_whisper)
 
-print(seperated_whisper_df.head())
-
 pho_output = pd.read_csv("pho_output.csv") #Be careful the pho_output.csv data should be in the same format as seperated_whisper_df --> file name / list of words / list of timestamps
+pho_output['pho'] = pho_output['pho'].apply(ast.literal_eval)
+pho_output['pho_timestamps'] = pho_output['pho_timestamps'].apply(ast.literal_eval)
 
 merged_df = pd.merge(seperated_whisper_df, pho_output, on='file_name', how='inner')
 
@@ -32,18 +32,23 @@ for _, row in merged_df.iterrows():
     timestamps = row['words_timestamps']
     index2 = 0 
     pho_row_result = []
+    
     for index, word in enumerate(words):
         list_of_pho = []
-        while index2 < len(row["pho_timestamps"]) and ((timestamps[index][0] <= row["pho_timestamps"][index2][0] <= timestamps[index][1]) or (timestamps[index][0] <= row["pho_timestamps"][index2][1] <= timestamps[index][1])):
-            list_of_pho.append(row["pho"][index2])
-            if index2+1 < len(row["pho_timestamps"]):
-                if not((timestamps[index][0] <= row["pho_timestamps"][index2+1][0] <= timestamps[index][1]) or (timestamps[index][0] <= row["pho_timestamps"][index2+1][1] <= timestamps[index][1])):
-                    break
+        length = len(row["pho_timestamps"])
+        
+        while index2 < length and row["pho_timestamps"][index2][0] <= timestamps[index][1]:
+            if ((timestamps[index][0] <= row["pho_timestamps"][index2][0] <= timestamps[index][1]) or (timestamps[index][0] <= row["pho_timestamps"][index2][1] <= timestamps[index][1])):
+                list_of_pho.append(row["pho"][index2])
             index2+=1
+
+        if index2-1 >= 0 and row["pho"][index2-1] not in list_of_pho:
+            if ((timestamps[index][0] <= row["pho_timestamps"][index2-1][0] <= timestamps[index][1]) or (timestamps[index][0] <= row["pho_timestamps"][index2-1][1] <= timestamps[index][1])):
+                list_of_pho.append(row["pho"][index2-1])
+        
         pho_row_result.append(list_of_pho)
+        
     result.append({'file_name': row['file_name'], 'words': words, 'pho': pho_row_result})
     
 result = pd.DataFrame(result)
-                
-
-   
+result.to_csv("combined_output.csv", index=False)
