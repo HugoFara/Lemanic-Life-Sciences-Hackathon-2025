bb0a3d90684d35fdc01f3f332918a6b885f9e89f
HugoFara <hugo.farajallah@unige.ch>
2025-05-01 00:13:13 +0200

    fix: integrated pipeline, adapting more segments. The training is not ready yet for the two coders version.

--- data_processing.py
+++ data_processing.py
@@ -1,48 +1,40 @@
 import pandas as pd
 import ipa_encoder
 
-language = "fr"
 
-files_path = {
-    "it": "Hackathon_ASR/1_Ground_truth/Decoding_ground_truth_IT.csv", 
-    "fr": "Hackathon_ASR/1_Ground_truth/Phoneme_Deleletion_ground_truth_FR.csv"
-}
+def get_phonemes(dataframe):
+    new_dataframe = dataframe.copy()
+    new_dataframe = new_dataframe.drop([
+        "participant_id", "phase", "language", "form", "config", "config_id", "API_target",
+        "accuracy_coder1", "notes_coder1", "accuracy_coder2", "notes_coder2"
+    ], axis=1)
 
-dataframe = pd.read_csv(files_path[language])
-new_dataframe = dataframe.copy()
-new_dataframe = new_dataframe.drop([
-    "participant_id", "phase", "language", "form", "config", "config_id", "API_target",
-    "accuracy_coder1", "notes_coder1", "accuracy_coder2", "notes_coder2"
-], axis=1)
+    for coder in ["coder1", "coder2"]:
+        print("Preparing data for", coder)
 
-for coder in ["coder1", "coder2"]:
-    print("Preparing data for", coder)
+        labeled_phonemes = dataframe["trial_answer_" + coder]
 
-    labeled_phonemes = dataframe["trial_answer_" + coder]
+        # Remove all symbols
+        labeled_phonemes = (
+            labeled_phonemes.str
+            # Replace space with a dot
+            .replace(r'[ \s ]+', '.', regex=True)
+            # Replace <...>, {...} with [UNK]
+            .replace(r'(\{.*?\}|\<.*?\>)', '?', regex=True)
+        )
 
-    # Remove all symbols
-    labeled_phonemes = (
-        labeled_phonemes.str
-        # Replace space with a dot
-        .replace(r'[ \s ]+', '.', regex=True)
-        # Replace <...>, {...} with [UNK]
-        .replace(r'(\{.*?\}|\<.*?\>)', '?', regex=True)
-    )
+        new_phonemes = ipa_encoder.get_ipa(
+            [str(sentence) for sentence in labeled_phonemes],
+            language
+        )
+        new_phonemes = [
+            sentence.strip().replace(".", "[PAD]").replace("?", "[UNK]")
+            for sentence in new_phonemes
+        ]
 
-    new_phonemes = ipa_encoder.get_ipa(
-        [str(sentence) for sentence in labeled_phonemes],
-        language
-    )
-    new_phonemes = [
-        sentence.strip().replace(".", "[PAD]").replace("?", "[UNK]")
-        for sentence in new_phonemes
-    ]
-
-    new_dataframe[f"phonemes_{coder}"] = pd.Series(new_phonemes)
-    new_dataframe = new_dataframe.drop([f"trial_answer_{coder}"], axis=1)
-
-output_path = f"outputs/phonemized_{language}.csv"
-new_dataframe.to_csv(output_path, index=False)
+        new_dataframe[f"phonemes_{coder}"] = pd.Series(new_phonemes)
+        new_dataframe = new_dataframe.drop([f"trial_answer_{coder}"], axis=1)
+    return new_dataframe
 
 
 """
@@ -57,4 +49,17 @@ for coder in ["coder1", "coder2"]:
 
     labeled_scores = data[accuracy]
 """
+
+
+if __name__ == "__main__":
+    language = ("fr", "it")[0]
+    files_path = {
+        "it": "Hackathon_ASR/1_Ground_truth/Decoding_ground_truth_IT.csv", 
+        "fr": "Hackathon_ASR/1_Ground_truth/Phoneme_Deleletion_ground_truth_FR.csv"
+    }
+    dataframe = pd.read_csv(files_path[language])
+
+    new_dataframe = get_phonemes(dataframe)
+    output_path = f"datasets/phonemized_{language}.csv"
+    new_dataframe.to_csv(output_path, index=False)
 
