4f569786c610ed890ea6dbbc95d13f5233d6f83a
HugoFara <hugo.farajallah@unige.ch>
2025-04-30 22:25:47 +0200

    refactor(phonemization): streamlining the pipeline.

--- data_processing.py
+++ data_processing.py
@@ -8,37 +8,33 @@ files_path = {
     "fr": "Hackathon_ASR/1_Ground_truth/Phoneme_Deleletion_ground_truth_FR.csv"
 }
 
+data = pd.read_csv(files_path[language])
+
 for coder in ["coder1", "coder2"]:
-    data = pd.read_csv(files_path[language])
     print("Preparing data for", coder)
 
-    trial_answer = "trial_answer_" + coder
-    labeled_phonemes = data[trial_answer]
+    labeled_phonemes = data["trial_answer_" + coder]
 
     # Remove all symbols
-    labeled_phonemes = (labeled_phonemes
-        .str.replace('.', ' ', regex=False)               # Replace dots with [PAD]
-        .str.replace(r'\{.*?\}', '[UNK]', regex=True)     # Replace {...} with [UNK]
-        .str.replace(r'\<.*?\>', '', regex=True)          # Remove <...>
+    labeled_phonemes = (
+        labeled_phonemes.str
+        .replace('.', ' ', regex=False)               # Replace dots with [PAD]
+        .replace(r'\{.*?\}', '[UNK]', regex=True)     # Replace {...} with [UNK]
+        .replace(r'\<.*?\>', '', regex=True)          # Remove <...>
     )
 
     new_phonemes = []
     for sentence in labeled_phonemes:
         words = sentence.split()
-        ipa_words = [ipa_encoder.get_ipa(word, language) for word in words]
-        ipa_output = ["".join(item for sublist in ipa_encoder.get_ipa(word, language) for item in sublist) for word in words]
-        new_sentence = " ".join(ipa_output)
-        new_phonemes.append(new_sentence)
-    
-    # Replace spaces with [PAD]
+        ipa_words = ipa_encoder.get_ipa(words, language)
+        new_phonemes.append("[PAD]".join(ipa_words))
+
     labeled_phonemes = pd.Series(new_phonemes)
-    labeled_phonemes = labeled_phonemes.str.replace(' ', '[PAD]', regex=False)     
 
     new_data = data.copy()
-    new_data["trial_answer_coder1"] = labeled_phonemes
-    output_path = "outputs/" + coder
+    new_data[f"trial_answer_phonemes"] = labeled_phonemes
+    output_path = f"outputs/phonemized_{language}_{coder}"
     # new_data.to_csv(output_path, index=True)
-
 
 
 """
