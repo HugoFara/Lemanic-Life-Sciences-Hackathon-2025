e57cd562b1f9e25365d99ce088e742e6354d13a1
Hugo Farajallah <Hugo.Farajallah@unige.ch>
2025-05-07 11:57:59 +0200

    feat: more robust model training.

--- src/phonemizer/commonvoice.py
+++ src/phonemizer/commonvoice.py
@@ -5,20 +5,36 @@ import huggingface_hub
 
 from . import text_phonemizer
 
-LIMIT_ITEMS = 30
+LIMIT_ITEMS = 3000
 LANGUAGES = ("fr", "it")
 
 
 def get_phonemized_datasets(languages, limit_items=LIMIT_ITEMS):
+    """Get a fully phonemized dataset from common voice."""
     with open("config.json", "r") as file:
         config = json.load(file)
         huggingface_hub.login(config["hf_token"])
 
     ds = {}
+    partial = ""
+    if limit_items != -1:
+        partial = f"[:{limit_items}]"
+
     for language in languages:
-        common_voice_ds = datasets.load_dataset("mozilla-foundation/common_voice_17_0", language, split=f"train[:{limit_items}]").remove_columns(["client_id", "path", "up_votes", "down_votes", "gender", "locale", "segment", "variant"])
+        common_voice_ds = datasets.load_dataset(
+            "mozilla-foundation/common_voice_17_0",
+            language,
+            split=f"train{partial}"
+        ).remove_columns(
+            ["client_id", "path", "up_votes", "down_votes", "gender", "locale", "segment", "variant"]
+        )
         # audio and sentence
-        ds[language] = text_phonemizer.phonemized_dataset(common_voice_ds, language, ["sentence"], ["target_phonemes1"])
+        ds[language] = text_phonemizer.phonemized_dataset(
+            common_voice_ds,
+            language,
+            ["sentence"],
+            ["target_phonemes1"]
+        )
     return ds
 
 
